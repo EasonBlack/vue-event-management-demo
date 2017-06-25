b61b57f5bc7ec21f9f616dc9d7f7f6fbecb71a32
EasonBlack <eason820720@163.com>
2017-06-25 22:28:46 +0800

    code split

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue-event-management-demo
 demo
+
+## Build Setup
+
+```shell
+# install dependencies
+$ npm i
+
+# serve with hot reload at http://localhost:8080
+$ npm run dev
+```
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
-
-import Calendar from './modules/calendar/index.vue';
+const Calendar = resolve => require(['./modules/calendar/index.vue'], resolve)
 
 const router = new VueRouter({
     routes: [
